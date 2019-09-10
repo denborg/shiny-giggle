beecbe0ceffa898c82704a8555653539ddee20ab
denborg <denborgg@gmail.com>
2019-09-10 19:03:41 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 import socket
+ipad = input()
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind(('192.168.0.37',8081))
-client = []
+sock.bind((ipad,8081))
+client = [] # Массив где храним адреса клиентов
 print ('Start Server')
 while 1 :
         data, addres = sock.recvfrom(1024)
         print(addres[0], addres[1])
         if  addres not in client : 
-                client.append(addres)
+                client.append(addres)# Если такова клиента нету , то добавить
         for clients in client :
                 if clients == addres : 
-                    continue
+                    continue # Не отправлять данные клиенту который их прислал
                 sock.sendto(data,clients)
